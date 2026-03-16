5b4bbe12d45fead2e1631e7b8f1d7acc133f8cd9
Manoj Royal <manojroyal7805@gmail.com>
2026-03-16 14:50:19 +0530

    Changed the dataset, cleaned it and trained the data for which I got an accuracy of 86%

--- model/train_model.py
+++ model/train_model.py
@@ -1,22 +1,37 @@
 import pandas as pd
 import pickle
 
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-data = pd.read_csv("Q:/Projects/ai-ticket-auto-resolution/data/tickets.csv")
+df = pd.read_csv(r"Q:\Projects\ai-ticket-auto-resolution\data\final_it_tickets.csv")
+print("Dataset loaded")
 
-X = data["ticket"]
-y = data["category"]
+X = df["ticket"]
+y = df["category"]
 
-vectorizer = TfidfVectorizer()
-model = LogisticRegression()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.23, random_state = 42, stratify = y)
 
-X_vectors = vectorizer.fit_transform(X)
-model.fit(X_vectors, y)
+##Converting the text into vectors
+vectorizer = TfidfVectorizer(stop_words = "english", max_features = 20000, ngram_range=(1,2), min_df=2)
 
-## Saving the model
-pickle.dump(model, open("model.pkl", "wb"))
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+print("Vectorization complete")
 
-## Saving hte vectorizer
-pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
+model = LogisticRegression(max_iter = 1000, class_weight="balanced")
+model.fit(X_train_vec, y_train)
+print("Model training is completed")
+
+y_pred = model.predict(X_test_vec)
+
+print("\nAccuracy: ", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n")
+print(classification_report(y_test, y_pred))
+
+pickle.dump(model, open("model/model.pkl", "wb"))
+pickle.dump(vectorizer, open("model/vectorizer.pkl", "wb"))
+
+print("\nModel saved successfully!")
